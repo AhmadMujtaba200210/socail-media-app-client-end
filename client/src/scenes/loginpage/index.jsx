d275ae037d2c25936c79512cc8ed1a01b19a791e
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-01 22:55:23 +0500

    react-redux folder architecture set

---
+++ client/src/scenes/loginpage/index.jsx
@@ -0,0 +1,7 @@
+const LoginPage=()=>{
+    return (
+        <div>homepage</div>
+    )
+}
+
+export default LoginPage;
---
+++ client/src/scenes/navabar/index.jsx
@@ -0,0 +1,7 @@
+const Navbar=()=>{
+    return (
+        <div>homepage</div>
+    )
+}
+
+export default Navbar;
