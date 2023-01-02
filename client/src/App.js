db7cfd6507ebd50c4d9d57071787d0c8ffd0a488
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-02 12:55:26 +0500

    feat: set route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import ProfilePage from "scenes/profilepage";
+import LoginPage from "scenes/loginpage";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
