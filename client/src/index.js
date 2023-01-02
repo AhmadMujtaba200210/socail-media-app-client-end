844716caf16c69fa802e5fd2674359a98880956b
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-02 15:41:26 +0500

    feat: setting redux

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ const initialState={
   posts:[],
 }
 
+//setting the local state of the browser
 
 export const authSlice=createSlice({
   name: "auth",
@@ -19,14 +20,23 @@ export const authSlice=createSlice({
   reducers:{
     setMode:(state)=>{
       state.mode=state.mode==="light" ? "dark" : "light";
-    },
+    }, 
     setLogin:(state,action)=>{
       state.user=action.payload.user;
       state.token=action.payload.token;
     },
     setLogout:(state)=>{
       state.user=null;
-      state.token=null;
+      state.token=null;   
+    },
+    setFriends:(state,action)=>{
+      if(state.user)
+      state.user.friends=action.payload.friends;
+      else
+      console.error("user friends  non existent :(");
+    },
+    setPosts: (state,action)=>{
+      state.posts=action.payload.posts;
     }
   }
 })
