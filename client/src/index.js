d0a05e1cbc0e3f2027f945df8eab718583bd40fb
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-03 23:28:51 +0500

    bug: filled missed error

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import authReducer from "./state";
-import { persistStore,persistReducer,FLUSH,REHYDRATE,PAUSE,REGISTER,PURGE } from 'redux-persist';
+import { persistStore,persistReducer,FLUSH,REHYDRATE,PAUSE,PERSIST,REGISTER,PURGE } from 'redux-persist';
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from 'redux-persist/integration/react';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 
 
-//setting up the redux state store to store the login user state in local storage as cached memory
+//  setting up the redux state store to store the login user state in local storage as cached memory
 // all of this setting information is taken from the documentation of redux-toolkit and redux-persist
 const persistConfig={key: "root", storage, version: 1};
 const persistedReducer= persistReducer(persistConfig,authReducer);
@@ -18,7 +18,7 @@ const store=configureStore({
   reducer: persistedReducer,
   middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
     serializableCheck:{
-      ignoredActions: [FLUSH,REHYDRATE,PAUSE,REGISTER,PURGE],
+      ignoredActions: [FLUSH,REHYDRATE,PAUSE,PERSIST,REGISTER,PURGE],
     },
   }),
 });
