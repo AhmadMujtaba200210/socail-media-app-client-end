3e8eced3701d61f2de80b1d07a0ecc5b2f12ffc7
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-05 00:47:08 +0500

    feat: styling navbar

--- client/src/scenes/navabar/index.jsx
+++ client/src/scenes/navabar/index.jsx
@@ -65,6 +65,33 @@ const Navbar = () => {
                     </FlexBetween>
                 )}
             </FlexBetween>
+            {/* Desktop Nav */}
+            {isNonMobileScreen ?
+                (<FlexBetween gap="2rem">
+                    <IconButton onClick={()=>dispatch(setMode())}>
+                        {theme.palette.mode==="dark"?(
+                            <DarkMode sx={{fontSize:"25px"}}/>
+                        ):(<LightMode sx={{color:dark, fontSize:"25px"}}/>)}
+                    </IconButton>
+                    <Message sx={{fontSize:"25px"}}/>
+                    <Notifications sx={{fontSize:"25px"}}/>
+                    <Help sx={{fontSize:"25px"}}/>
+                    <FormControl variant="standard" value={fullName}>
+                        <Select
+                            value={fullName}
+                            sx={{
+                                backgroundColor:neutralLight,
+                                width:"150px",
+                                borderRadius:"0.25rem",
+                                
+                            }}
+                            >
+
+                        </Select>
+                    </FormControl>
+                </FlexBetween>)
+                :(<IconButton></IconButton>)
+            }
         </FlexBetween>
     )
 }
