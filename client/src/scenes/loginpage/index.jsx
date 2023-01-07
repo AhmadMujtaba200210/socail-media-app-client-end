8e0df68f797722948b682d41374144154eaf3755
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-07 18:37:20 +0500

    feat: created design for form

--- client/src/scenes/loginpage/index.jsx
+++ client/src/scenes/loginpage/index.jsx
@@ -1,7 +1,35 @@
+import { Box, Typography, useTheme,useMediaQuery } from "@mui/material";
+import Form from "./Form"
+
 const LoginPage=()=>{
-    return (
-        <div>homepage</div>
-    )
+    const theme=useTheme;
+    const isNonMobileScreens=useMediaQuery("(min-width:1000)")
+    return <Box>
+        <Box
+            width="100%"
+            backgroundColor={theme.palette.background.alt}
+            p="1rem 6%"
+            textAlign="center"
+        >
+        <Typography
+            fontWeight="bold"
+            fontSize="32px"
+            color="primary"
+        >Casepedia</Typography>
+        </Box>
+        <Box
+            width={isNonMobileScreens? "50%":"100%"}
+            p="2rem"
+            m="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{mb:"1.5rem"}} >
+                Welcome To CASEmedia, The Social Media for CASE Students
+            </Typography>
+            <Form/>
+        </Box>
+    </Box>
 }
 
 export default LoginPage;
